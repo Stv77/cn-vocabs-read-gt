99e8c858660a7c7877ea5e06643335a2c43a880d
Stevan <67777246+Stv77@users.noreply.github.com>
2022-06-23 18:02:51 +0700

    driver-updated

--- driver.py
+++ driver.py
@@ -1,24 +1,5 @@
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import time
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
-
-#read bbc uk article for 1 minute
-def search_on_bbc():
-    driver.get('https://www.bbc.co.uk/search')
-    input = driver.find_element_by_css_selector('input#search-input')
-    input.click()
-    input.send_keys(Keys.CONTROL, 'v')
-    input.send_keys(Keys.ENTER)
-    time.sleep(60)
-
-#Read Chinese New York Times
-def search_on_nyt():
-    driver.get('https://cn.nytimes.com/')
-    input = driver.find_element_by_css_selector('input.searchQuery')
-    input.click()
-    input.send_keys(Keys.CONTROL, 'v')
-    input.send_keys(Keys.ENTER)
-    time.sleep(60)
